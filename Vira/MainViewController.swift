0508d3b305c1e6bd2f1f4dda9bf60524a704e196
Jack Cook <hello@jackcook.nyc>
2019-01-25 14:03:32 -0500

    Initial commit

---
+++ Vira/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  Vira
+//
+//  Created by Jack Cook on 1/25/19.
+//  Copyright © 2019 Jack Cook. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
